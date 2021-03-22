3909a39f8435f1461f8c570d9d4ff20b9fbdc69b
darrickpang <dkypang@yahoo.com>
2021-03-21 18:02:26 -0700

    has many

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+    has_many :comments
+    has_many :movie_users
+    has_many :users, through: :movie_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :comments
+    has_many :movie_users
+    has_many :movies, through: :movie_users
+
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+
+    validates :name, uniqueness: true, presence: true
 end
