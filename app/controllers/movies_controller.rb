ecc7c8af548ebb04cad3962ecaddbe016f2c6c4a
darrickpang <dkypang@yahoo.com>
2021-03-21 17:50:59 -0700

    movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,42 @@
 class MoviesController < ApplicationController
+    def index
+        movies = Movie.all 
+        render json: movies
+    end 
+
+    def new
+        movie = Movie.new
+    end
+
+    def create 
+        movie = Movie.create(movie_params)
+        render json: movie
+    end
+
+    def show
+        movie = Movie.find(params[:id])
+        render json: movie
+    end
+
+    def edit
+        movie = Movie.find(params[:id])
+    end
+
+    def update 
+        movie = Movie.find(params[:id])
+        movie.update(movie_params)
+        render json: movie
+    end
+
+    def destroy 
+        movie = Movie.find(params[:id])
+        tempDate = movie
+        movie.destroy
+        render json: tempDate
+    end 
+
+    private
+    def movie_params
+        params.require(:movie).permit(:name, :year, :poster)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Movie.destroy_all 
+
+User.create(name: 'Darrick Pang', password: '123')
+
+Movie.create(name: `Zack Synder's Justice League`, year: '2021', poster: 'https://images-na.ssl-images-amazon.com/images/I/61FsK3k4N7L._AC_SL1024_.jpg')
+Movie.create(name: 'Alien: Covenant', year: '2017', poster: 'https://lumiere-a.akamaihd.net/v1/images/image_f9126949.jpeg')
+Movie.create(name: 'World War Z', year: '2013', poster: 'https://www.paramountmovies.com/assets/posters/worldwarz_dvd_en_800x1200.jpg')
+Movie.create(name: 'Tears of the Sun', year: '2003', poster: 'https://images-na.ssl-images-amazon.com/images/I/51zZKJyN8cL._SY445_.jpg')
