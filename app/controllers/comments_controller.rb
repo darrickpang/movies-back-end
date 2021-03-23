ff81cadf36df71d695c7caafa2ddd3918f9f593d
darrickpang <dkypang@yahoo.com>
2021-03-22 17:59:28 -0700

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,42 @@
 class CommentsController < ApplicationController
+    def index
+        comments = Comment.all 
+        render json: comments
+    end 
+
+    def new
+        comment = Comment.new
+    end
+
+    def create 
+        comment = Comment.create(comment_params)
+        render json: comment
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment
+    end
+
+    def edit
+        comment = Comment.find(params[:id])
+    end
+
+    def update 
+        comment = Comment.find(params[:id])
+        comment.update(comment_params)
+        render json: comment
+    end
+
+    def destroy 
+        comment = Comment.find(params[:id])
+        tempDate = comment
+        comment.destroy
+        render json: tempDate
+    end 
+
+    private
+    def comment_params
+        params.require(:comment).permit(:comment, :user_id, movie_id)
+    end
 end
